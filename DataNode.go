a0c5eee4830b0eb77102acb15fdc6359c041bedc
Amr Salahuddin <120669828+amr-salahuddin@users.noreply.github.com>
2025-04-10 21:12:47 +0200

    Add files via upload

---
+++ DataNode.go
@@ -0,0 +1,197 @@
+package main
+
+import (
+	"context"
+	"log"
+	"path/filepath"
+	"strings"
+	"time"
+	"fmt"
+	"net"
+	"os"
+	pb "proj/Services"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+
+)
+
+const (
+	port                   = ":50052"
+	masterAddress          = "localhost:50060" // Address of the master node
+	maxGRPCSize       = 1024 * 1024 * 100 // 100 MB
+	id               int32 = 0
+
+)
+
+type dataNodeServer struct {
+	pb.UnimplementedFileServiceServer
+}
+
+// Handles file upload from a client node
+
+func (d *dataNodeServer) UploadFile(ctx context.Context, in *pb.FileUploadRequest) (*pb.FileUploadResponse, error) {
+	log.Printf("Received upload request for: %s", req.FileName)
+
+	// Metadata extraction (client IP and port)
+	md, exists := metadata.FromIncomingContext(ctx)
+	if !exists {
+		log.Println("No metadata in request")
+	}
+	clientIP := strings.Join(md.Get("client-ip"), ",")
+	clientPort := strings.Join(md.Get("client-port"), ",")
+
+
+outMeta := metadata.Pairs("client-ip", clientIP, "client-port", clientPort)
+	outCtx := metadata.NewOutgoingContext(context.Background(), outMeta)
+
+	
+	// Save directory for this DataNode
+	nodeDir := fmt.Sprintf("./uploaded_%d", dataNodeID)
+	if err := os.MkdirAll(nodeDir, 0755); err != nil {
+		return nil, fmt.Errorf("error creating upload dir: %v", err)
+	}
+	
+	// File saving path
+	savePath := filepath.Join(nodeDir, req.FileName+".mp4")
+	file, err := os.Create(savePath)
+	if err != nil {
+		return nil, fmt.Errorf("error creating file: %v", err)
+	}
+	defer file.Close()
+
+	if _, err := file.Write(req.FileContent); err != nil {
+		return nil, fmt.Errorf("error writing file content: %v", err)
+	}
+
+	log.Printf("File stored at: %s", savePath)
+
+
+	// Write the content to the file
+
+	log.Printf("File uploaded success at %s", filePath)
+	
+
+	 // Asynchronously notify the master node about the upload
+	 go notifyMasterOfUpload(outCtx, req.FileName, savePath)
+
+	return &pb.FileUploadResponse{Message: "Upload successful"}, nil
+}
+
+func notifyMasterOfUpload(ctx context.Context, filename, path string) {
+	conn, err := grpc.Dial(masterAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Printf("Failed to notify master: %v", err)
+		return
+	}
+	defer conn.Close()
+
+	client := pb.NewFileServiceClient(conn)
+
+	_, err = client.NotifyUploaded(ctx, &pb.NotifyUploadedRequest{
+		FileName: filename,
+		DataNode: id,
+		FilePath: path,
+	})
+	if err != nil {
+		log.Printf("Master notification failed: %v", err)
+	}
+}
+
+
+func (d *dataNodeServer) DownloadFile(ctx context.Context, in *pb.FileDownloadRequest) (*pb.FileDownloadResponse, error) {
+	log.Printf("FileDownloadRequest %s", in.FileName)
+	dir := fmt.Sprintf("./uploaded_%d", id)
+
+	filePath := filepath.Join(dir, in.FileName+".mp4")
+
+	fileContent, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("ReadFile fail %v", err)
+	}
+	// Create and return the response with the file content
+	response := &pb.FileDownloadResponse{
+		FileContent: fileContent,
+	}
+	return response, nil
+}
+
+func (d *dataNodeServer) sendHeartbeat() {
+	masterConn, err := grpc.Dial(masterAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Cannot connect to Master %v", err)
+	}
+	defer masterConn.Close()
+	masterClient := pb.NewFileServiceClient(masterConn)
+
+	for {
+		time.Sleep(time.Second) 
+
+		keepAliveRequest := &pb.KeepAliveRequest{
+			DataNode: fmt.Sprintf("%d", id), 
+			IsAlive:  1,                   
+		}
+
+		_, err := masterClient.KeepAlive(context.Background(), keepAliveRequest)
+		if err != nil {
+			log.Printf("Cannot Send KeepAlive %v", err)
+		}
+	}
+}
+func (d *dataNodeServer) Replicate(ctx context.Context, req *pb.ReplicateRequest) (*pb.ReplicateResponse, error) {
+
+	log.Printf("Replicating file: %s to %d node(s)", req.FileName, len(req.IpAddresses))
+
+
+    // Read the content of the file
+	content, err := os.ReadFile(req.FilePath)
+	if err != nil {
+		return nil, fmt.Errorf("replication failed, cannot read file: %v", err)
+	}
+
+    // Iterate over the provided IP addresses and ports
+    for i, ip := range in.IpAddresses {
+
+		addr := fmt.Sprintf("%s:%d", ip, req.PortNumbers[i])
+
+		conn, err := grpc.Dial(addr, grpc.WithInsecure())
+		if err != nil {
+			log.Printf("Connection failed to %s: %v", addr, err)
+			continue
+		}
+		defer conn.Close()
+
+		client := pb.NewFileServiceClient(conn)
+
+        // Invoke the UploadFile service
+		_, err = client.UploadFile(ctx, &pb.FileUploadRequest{
+			FileName:    req.FileName,
+			FileContent: content,
+		})
+		if err != nil {
+			log.Printf("Replication upload failed to %s: %v", addr, err)
+		} else {
+			log.Printf("Replicated to %s", addr)
+		}
+
+    }
+
+    return &pb.ReplicateResponse{}, nil
+}
+
+
+func main() {
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("tcp listen fail %v", err)
+	}
+
+
+	grpcServer := grpc.NewServer(grpc.MaxRecvMsgSize(maxGRPCSize))
+	dataServer := &dataNodeServer{}
+	pb.RegisterFileServiceServer(grpcServer, dataServer)
+	log.Printf("DataNode running at %s", listenPort)
+	go grpcServer.Serve(listener) 
+
+	go dataServer.sendHeartbeat()
+	select {}
+}
---
+++ MasterNode.go
@@ -0,0 +1,347 @@
+package main
+
+import (
+	pb "proj/Services"
+	"context"
+	"errors"
+	"log"
+	"math/rand"
+	"time"
+	"fmt"
+	"net"
+	"strconv"
+	"sync"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
+)
+
+const (
+	port             = ":50060"
+	keepAliveTimeout = 2* time.Second 
+)
+
+type FileRecord struct {
+	FileName  string
+	FilePaths []string
+	DataNodes []int32
+}
+
+type MachineRecord struct {
+	IPAddress      string
+	AvailablePorts []int32
+	Liveness       bool
+}
+
+type server struct {
+	fileRecords      map[string]*FileRecord
+	machineRecords   []*MachineRecord
+	lastKeepAliveMap map[int]time.Time 
+	mutex            sync.Mutex
+	pb.UnimplementedFileServiceServer
+}
+
+func (s *server) PrintMachineRecords() {
+	fmt.Println("Machine Records:")
+	for i, record := range s.machineRecords {
+		fmt.Printf("Machine %d:\n  IP: %s\n  Alive: %t\n  Ports: %v\n", i, record.IPAddress, record.Liveness, record.AvailablePorts)
+	}
+}
+
+func (s *server) PrintFileRecords() {
+	fmt.Println("File Records:")
+	for filename, record := range s.fileRecords {
+		fmt.Printf("File: %s\n  Paths: %v\n  Nodes: %v\n", filename, record.FilePaths, record.DataNodes)
+	}
+}
+
+func (s *server) HandleUploadFile(ctx context.Context, in *pb.HandleUploadFileRequest) (*pb.HandleUploadFileResponse, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	aliveMachines := make([]*MachineRecord, 0)
+	for _, machine := range s.machineRecords {
+		if machine.Liveness {
+			aliveMachines = append(aliveMachines, machine)
+		}
+	}
+	if len(aliveMachines) == 0 {
+		return nil, errors.New("no aliveMachines")
+	}
+
+	selectedMachine := aliveMachines[rand.Intn(len(aliveMachines))]
+
+	selectedPort := selectedMachine.AvailablePorts[rand.Intn(len(selectedMachine.AvailablePorts))]
+	selectedIP := selectedMachine.IPAddress
+
+	response := &pb.HandleUploadFileResponse{
+		PortNumber: selectedPort,
+		IpAddress:  selectedIP,
+	}
+
+	return response, nil
+}
+
+func (s *server) HandleDownloadFile(ctx context.Context, in *pb.HandleDownloadFileRequest) (*pb.HandleDownloadFileResponse, error) {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	fileRecord, ok := s.fileRecords[in.FileName]
+	if !ok {
+		return nil, errors.New("No such filename exist")
+	}
+
+	var ipAddresses []string
+	var portNumbers []int32
+
+	for i, nodeID := range fileRecord.DataNodes {
+		if s.machineRecords[nodeID].Liveness {
+			datanode := s.machineRecords[fileRecord.DataNodes[i]]
+			ipAddresses = append(ipAddresses, datanode.IPAddress)
+			portNumbers = append(portNumbers, datanode.AvailablePorts[rand.Intn(len(datanode.AvailablePorts))])
+		}
+	}
+
+	response := &pb.HandleDownloadFileResponse{
+		IpAddress:   ipAddresses,
+		PortNumbers: portNumbers,
+	}
+
+	return response, nil
+}
+
+func (s *server) NotifyUploaded(ctx context.Context, in *pb.NotifyUploadedRequest) (*pb.NotifyUploadedResponse, error) {
+
+	rand.Seed(time.Now().UnixNano())
+
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if record, ok := s.fileRecords[in.FileName]; ok {
+		record.DataNodes = append(record.DataNodes, in.DataNode)
+		record.FilePaths = append(record.FilePaths, in.FilePath)
+
+		s.PrintFileRecords()
+		return &pb.NotifyUploadedResponse{}, nil
+	}
+
+	s.fileRecords[in.FileName] = &FileRecord{
+		FileName:  in.FileName,
+		FilePaths: []string{in.FilePath},
+		DataNodes: []int32{in.DataNode},
+	}
+
+		// Get client metadata
+
+	md, ok := metadata.FromIncomingContext(ctx)
+	if !ok {
+		log.Println("No metadata found in the context")
+	}
+	clientIP := md.Get("client-ip")
+	clientPort := md.Get("client-port")
+
+	fmt.Printf("Client IP: %s | Port: %s\n", clientIP, clientPort)
+
+
+	// Notify client asynchronously
+	go func() {
+		clientAddr := fmt.Sprintf("%s:%s", clientIP[0], clientPort[0])
+		conn, err := grpc.Dial(clientAddr, grpc.WithInsecure())
+		if err != nil {
+			log.Printf("Dial client fail %v", err)
+			return 
+		}
+		defer conn.Close()
+
+		client := pb.NewFileServiceClient(conn)
+
+		_, err := client.SendNotification(context.Background(), &pb.SendNotificationRequest{
+			Message: "File Upload Finish",
+		})
+		if err != nil {
+			log.Printf("SendNotification to client fail %v", err)
+		}
+	}()
+
+
+	// Trigger replication
+	sourceID := in.DataNode
+	var replicateIPs []string
+	var replicatePorts []int32
+	var replicateIds []int32
+
+	for i := 1; i <= 2; i++ {
+		replicateId := (sourceID + int32(i)) % int32(len(s.machineRecords))
+		if s.machineRecords[replicateId].Liveness {
+			replicateIPs = append(replicateIPs, s.machineRecords[replicateId].IPAddress)
+			replicatePorts = append(replicatePorts, s.machineRecords[replicateId].AvailablePorts[rand.Intn(len(s.machineRecords[replicateId].AvailablePorts))])
+			replicateIds = append(replicateIds, replicateId)
+		} 
+	}
+	replicateRequest := &pb.ReplicateRequest{
+		FileName:    in.FileName,
+		FilePath:    in.FilePath,
+		IpAddresses: replicateIPs,
+		PortNumbers: replicatePorts,
+		Ids:         replicateIds,
+	}
+
+	if s.machineRecords[sourceID].Liveness {
+		go func() {
+			clientAddress := fmt.Sprintf("%s%d", s.machineRecords[sourceID].IPAddress, s.machineRecords[sourceID].AvailablePorts[rand.Intn(len(s.machineRecords[sourceID].AvailablePorts))])
+			conn, err := grpc.Dial(clientAddress, grpc.WithInsecure())
+			if err != nil {
+				log.Printf("Dial source data node fail %v", err)
+				return
+			}
+			defer conn.Close()
+
+			sourceClient := pb.NewFileServiceClient(conn)
+
+			_, err = sourceClient.Replicate(context.Background(), replicateRequest)
+			if err != nil {
+				log.Printf("Replicate fail on source Datanode machine %v", err)
+				return
+			}
+		}()
+	}
+	s.PrintFileRecords()
+	return &pb.NotifyUploadedResponse{}, nil
+}
+
+
+// =======================
+// Background Processes
+// =======================
+
+func (s *server) replicationScheduler() {
+	for {
+		time.Sleep(10 * time.Second) 
+		s.mutex.Lock()
+
+		for _, fileRecord := range s.fileRecords {
+			var liveNodeIndexes []int 
+			for i, datanode := range fileRecord.DataNodes {
+				if s.machineRecords[datanode].Liveness {
+					liveNodeIndexes = append(liveNodeIndexes, i)
+				}
+			}
+			if len(liveNodeIndexes) < 2 && len(liveNodeIndexes) > 0 {
+
+				randomIndex := rand.Intn(len(liveNodeIndexes))
+				chosenNodeIndex := liveNodeIndexes[randomIndex]
+				sourceID := fileRecord.DataNodes[chosenNodeIndex]
+				var replicateIPs []string
+				var replicatePorts []int32
+				var replicateIds []int32
+				for i := 1; i <= 2; i++ {
+					replicateId := (sourceID + int32(i)) % int32(len(s.machineRecords))
+					isExist := false
+					for _, node := range fileRecord.DataNodes {
+						if node == replicateId {
+							isExist = true
+							break
+						}
+					}
+					if !isExist && s.machineRecords[replicateId].Liveness {
+						replicateIPs = append(replicateIPs, s.machineRecords[replicateId].IPAddress)
+						replicatePorts = append(replicatePorts, s.machineRecords[replicateId].AvailablePorts[rand.Intn(len(s.machineRecords[replicateId].AvailablePorts))])
+						replicateIds = append(replicateIds, replicateId)
+					} else {
+						log.Printf("machine %s not alive.", s.machineRecords[replicateId].IPAddress)
+					}
+				}
+				replicateRequest := &pb.ReplicateRequest{
+					FileName:    fileRecord.FileName,
+					FilePath:    fileRecord.FilePaths[chosenNodeIndex],
+					IpAddresses: replicateIPs,
+					PortNumbers: replicatePorts,
+					Ids:         replicateIds,
+				}
+				if s.machineRecords[sourceID].Liveness {
+
+					addr := fmt.Sprintf("%s%d", s.machineRecords[sourceID].IPAddress, s.machineRecords[sourceID].AvailablePorts[rand.Intn(len(s.machineRecords[sourceID].AvailablePorts))])
+
+					conn, err := grpc.Dial(addr, grpc.WithInsecure())
+					if err != nil {
+						log.Printf("Dial source data node fail %v", err)
+						continue
+					}
+					defer conn.Close()
+
+					sourceClient := pb.NewFileServiceClient(conn)
+
+					_, err = sourceClient.Replicate(context.Background(), replicateRequest)
+					if err != nil {
+						log.Printf("Replicate fail on source Datanode machine %v", err)
+						continue
+					}
+				}
+			}
+		}
+
+		s.mutex.Unlock()
+	}
+}
+
+func (s *server) monitorKeepAlive() {
+	ticker := time.NewTicker(keepAliveTimeout)
+	defer ticker.Stop()
+	for {
+		select {
+		case <-ticker.C:
+			s.mutex.Lock()
+			for nodeID, lastTime := range s.lastKeepAliveMap {
+				active := time.Since(lastTime) < keepAliveTimeout
+				s.machineRecords[nodeID].Liveness = active
+
+				log.Printf("DataNode #%d Active: %t", id, active)
+			}
+			s.mutex.Unlock()
+		}
+	}
+}
+
+func (s *server) KeepAlive(ctx context.Context, in *pb.KeepAliveRequest) (*pb.KeepAliveResponse, error) {
+	nodeID, err := strconv.Atoi(in.DataNode)
+	if err != nil {
+		return nil, fmt.Errorf("coversion to int fail %v", err)
+	}
+	log.Printf("Data node %d KeepAlive sent", nodeID)
+	s.mutex.Lock()
+	s.lastKeepAliveMap[nodeID] = time.Now()
+	defer s.mutex.Unlock()
+	return &pb.KeepAliveResponse{}, nil
+}
+
+func main() {
+	s := grpc.NewServer()
+
+	server := &server{
+		fileRecords:      make(map[string]*FileRecord),
+		machineRecords:   []*MachineRecord{},
+		lastKeepAliveMap: make(map[int]time.Time),
+	}
+
+	server.machineRecords = append(server.machineRecords, &MachineRecord{"localhost:", []int32{50052}, false})
+	server.machineRecords = append(server.machineRecords, &MachineRecord{"localhost:", []int32{50053}, false})
+	server.machineRecords = append(server.machineRecords, &MachineRecord{"localhost:", []int32{50054}, false})
+	server.machineRecords = append(server.machineRecords, &MachineRecord{"localhost:", []int32{50055}, false})
+
+	go server.monitorKeepAlive()
+
+	go server.replicationScheduler()
+
+	pb.RegisterFileServiceServer(s, server)
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("tcp listen fail: %v", err)
+	}
+	defer lis.Close()
+
+	log.Printf("listening on %s", port)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("s.Serve fail %v", err)
+	}
+}
